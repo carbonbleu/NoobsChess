be7b7e173e4bf1a00fd7dd1be69a8e06f43194db
unknown <axolotl.rocket@gmail.com>
2024-05-29 17:34:33 +0530

    Clean

--- NoobsChess/obj/Debug/net8.0/NoobsChess.AssemblyInfo.cs
+++ NoobsChess/obj/Debug/net8.0/NoobsChess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoobsChess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeec90e4ea002a0b960cfe76e92dcf9bde9406dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoobsChess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoobsChess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
